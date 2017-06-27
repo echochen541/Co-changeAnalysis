c37aaa692cd4ec7b15595f1560563389e78453e3
Echo Chen <echochen541@163.com>
2017-06-27 20:28:06 +0800

    get commit parent

--- src/main/java/cn/edu/fudan/se/cochange_analysis/git/extractor/GitExtractor.java
+++ src/main/java/cn/edu/fudan/se/cochange_analysis/git/extractor/GitExtractor.java
@@ -6,23 +6,42 @@ import java.util.Date;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 
 import cn.edu.fudan.se.cochange_analysis.git.bean.GitCommit;
+import cn.edu.fudan.se.cochange_analysis.git.bean.GitCommitParent;
 import cn.edu.fudan.se.cochange_analysis.git.bean.GitRepository;
 import cn.edu.fudan.se.cochange_analysis.git.dao.GitCommitDAO;
 
 public class GitExtractor {
-	private GitRepository repository;
+	private GitRepository gitRepository;
+	private Git git;
+	private Repository repository;
+	private RevWalk revWalk;
 
-	public GitExtractor(GitRepository repository) {
-		this.repository = repository;
+	public GitExtractor(GitRepository gitRepository) {
+		this.gitRepository = gitRepository;
+		try {
+			git = Git.open(new File(gitRepository.getRepositoryPath()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		repository = git.getRepository();
+		revWalk = new RevWalk(repository);
 	}
 
 	public void extractCommitHistory() {
-		Integer repositoryId = repository.getRepositoryId();
+		Integer gitRepositoryId = gitRepository.getRepositoryId();
 		try {
-			Git git = Git.open(new File(repository.getRepositoryPath()));
+			Git git = Git.open(new File(gitRepository.getRepositoryPath()));
 			Iterable<RevCommit> gitlog = git.log().call();
 			for (RevCommit revCommit : gitlog) {
 				String commitId = revCommit.getName();
@@ -35,20 +54,26 @@ public class GitExtractor {
 				String committerEmail = revCommit.getCommitterIdent().getEmailAddress();
 				Date committedDate = revCommit.getCommitterIdent().getWhen();
 
-				GitCommit commit = new GitCommit(repositoryId, commitId, shortMessage, fullMessage, authorName, authorEmail,
-						authoredDate, committerName, committerEmail, committedDate);
+				GitCommit commit = new GitCommit(gitRepositoryId, commitId, shortMessage, fullMessage, authorName,
+						authorEmail, authoredDate, committerName, committerEmail, committedDate);
 				System.out.println(commitId);
 				GitCommitDAO.insertCommit(commit);
 
 				// current commit has more than zero parent
 				if (revCommit.getParentCount() > 0) {
+					RevCommit[] parentRevCommits = revCommit.getParents();
 					// store parent commit
-
-					// only one parent
-					if (revCommit.getParentCount() == 1) {
-						RevCommit preRevCommit = revCommit.getParent(0);
-						extractChangedFiles(revCommit, preRevCommit);
+					for (RevCommit parentRevCommit : parentRevCommits) {
+						String parentCommitId = parentRevCommit.getName();
+						GitCommitParent commitParent = new GitCommitParent(gitRepositoryId, commitId, parentCommitId);
+						System.out.println(commitId + " " + commitParent);
 					}
+				}
+
+				// only one parent
+				if (revCommit.getParentCount() == 1) {
+					RevCommit preRevCommit = revCommit.getParent(0);
+					extractChangedFiles(revCommit, preRevCommit);
 				}
 			}
 		} catch (IOException | GitAPIException e) {
@@ -60,9 +85,34 @@ public class GitExtractor {
 		
 	}
 
+	// from the commit we can build the tree which allows us to construct the
+	// TreeParser
+	public CanonicalTreeParser prepareTreeParser(String objectId) {
+		CanonicalTreeParser treeParser = new CanonicalTreeParser();
+
+		try {
+			RevCommit commit = revWalk.parseCommit(ObjectId.fromString(objectId));
+			RevTree tree = revWalk.parseTree(commit.getTree().getId());
+			ObjectReader oldReader = repository.newObjectReader();
+			treeParser.reset(oldReader, tree.getId());
+			revWalk.dispose();
+		} catch (MissingObjectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IncorrectObjectTypeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return treeParser;
+	}
+
 	public static void main(String[] args) {
-		GitRepository repository = new GitRepository(2, "cassandra", "D:/echo/lab/research/co-change/projects/cassandra/.git");
-		GitExtractor gitExtractor = new GitExtractor(repository);
+		GitRepository gitRepository = new GitRepository(2, "cassandra",
+				"D:/echo/lab/research/co-change/projects/cassandra/.git");
+		GitExtractor gitExtractor = new GitExtractor(gitRepository);
 		gitExtractor.extractCommitHistory();
 	}
 }
