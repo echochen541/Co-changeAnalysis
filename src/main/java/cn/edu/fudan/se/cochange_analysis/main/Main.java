e431229430246b6859963d5cbf532bf3a4714d8f
Echo Chen <echochen541@163.com>
2017-07-10 18:55:35 +0800

    ready to extract change from repository 3,4,5,6

--- src/main/java/cn/edu/fudan/se/cochange_analysis/main/Main.java
+++ src/main/java/cn/edu/fudan/se/cochange_analysis/main/Main.java
@@ -7,41 +7,37 @@ import cn.edu.fudan.se.cochange_analysis.git.bean.GitRepository;
 public class Main {
 	public static void main(String[] args) {
 		// extract change operation
+		GitRepository gitRepository = new GitRepository(3, "cxf", "D:/echo/lab/research/co-change/projects/cxf/.git");
+		ChangeExtractor changeExtractor = new ChangeExtractor(gitRepository);
+		changeExtractor.extracChange();
+
+		gitRepository = new GitRepository(4, "hadoop", "D:/echo/lab/research/co-change/projects/hadoop/.git");
+		changeExtractor = new ChangeExtractor(gitRepository);
+		changeExtractor.extracChange();
+
+		gitRepository = new GitRepository(5, "hbase", "D:/echo/lab/research/co-change/projects/hbase/.git");
+		changeExtractor = new ChangeExtractor(gitRepository);
+		changeExtractor.extracChange();
+
+		gitRepository = new GitRepository(6, "wicket", "D:/echo/lab/research/co-change/projects/wicket/.git");
+		changeExtractor = new ChangeExtractor(gitRepository);
+		changeExtractor.extracChange();
+
+		// extract file pair
+		// System.out.println("Begin extract file pair:");
+		// GitRepository gitRepository = new GitRepository(1, "camel",
+		// "D:/echo/lab/research/co-change/projects/camel/.git");
+		// FilePairExtractor extractor = new FilePairExtractor(gitRepository);
+		// extractor.extractFilePairHistory();
+
+		// gitRepository = new GitRepository(2, "cassandra",
+		// "D:/echo/lab/research/co-change/projects/cassandra/.git");
+		// extractor = new FilePairExtractor(gitRepository);
+		// extractor.extractFilePairHistory();
 
 		// GitRepository gitRepository = new GitRepository(3, "cxf",
 		// "D:/echo/lab/research/co-change/projects/cxf/.git");
-		// ChangeExtractor changeExtractor = new ChangeExtractor(gitRepository);
-		// changeExtractor.extracChange();
-
-		// gitRepository = new GitRepository(4, "hadoop",
-		// "D:/echo/lab/research/co-change/projects/hadoop/.git");
-		// changeExtractor = new ChangeExtractor(gitRepository);
-		// changeExtractor.extracChange();
-
-		// gitRepository = new GitRepository(5, "hbase",
-		// "D:/echo/lab/research/co-change/projects/hbase/.git");
-		// changeExtractor = new ChangeExtractor(gitRepository);
-		// changeExtractor.extracChange();
-
-		// gitRepository = new GitRepository(6, "wicket",
-		// "D:/echo/lab/research/co-change/projects/wicket/.git");
-		// changeExtractor = new ChangeExtractor(gitRepository);
-		// changeExtractor.extracChange();
-
-		// extract file pair
-		System.out.println("Begin extract file pair:");
-		GitRepository gitRepository = new GitRepository(1, "camel",
-				"D:/echo/lab/research/co-change/projects/camel/.git");
-		FilePairExtractor extractor = new FilePairExtractor(gitRepository);
-		extractor.extractFilePairHistory();
-
-		gitRepository = new GitRepository(2, "cassandra", "D:/echo/lab/research/co-change/projects/cassandra/.git");
-		extractor = new FilePairExtractor(gitRepository);
-		extractor.extractFilePairHistory();
-
-		// gitRepository = new GitRepository(3, "cxf",
-		// "D:/echo/lab/research/co-change/projects/cxf/.git");
-		// extractor = new FilePairExtractor(gitRepository);
+		// FilePairExtractor extractor = new FilePairExtractor(gitRepository);
 		// extractor.extractFilePairHistory();
 
 		// gitRepository = new GitRepository(4, "hadoop",
