8a7c52a09068fb365d2db3cf2e9744c9103ada8a
Echo Chen <echochen541@163.com>
2017-07-12 10:16:18 +0800

    add jiracrawler, issue bug bean dao mapper

---
+++ src/main/java/cn/edu/fudan/se/cochange_analysis/crawler/JIRACrawler.java
@@ -0,0 +1,15 @@
+package cn.edu.fudan.se.cochange_analysis.crawler;
+
+import cn.edu.fudan.se.cochange_analysis.git.bean.GitRepository;
+
+public class JIRACrawler {
+	private GitRepository repository;
+	private String beginURL;
+	private int pageNum;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
