ebc772cc04f050e17c26abaad7a3aa2a95bb9359
Echo Chen <echochen541@163.com>
2017-07-18 17:58:15 +0800

    refactor GenerateDSM, RelationParser

--- src/main/java/cn/edu/fudan/se/cochange_analysis/file/util/GenerateDSM.java
+++ src/main/java/cn/edu/fudan/se/cochange_analysis/file/util/GenerateDSM.java
@@ -15,74 +15,74 @@ public class GenerateDSM {
 	private int[][] historyDsmMatrix;
 	private List<String> typeList;
 	private List<String> fileList;
-	
+
 	private StringBuilder matrixBuilder;
 	private StringBuilder typeBuilder;
 	private StringBuilder fileListBuilder;
-	
+
 	private StringBuilder matrixCell;
-	
-	public GenerateDSM(List<String> mTypeList,List<String> mFileList){
-		
+
+	public GenerateDSM(List<String> mTypeList, List<String> mFileList) {
+
 		Collections.sort(mTypeList);
 		Collections.sort(mFileList);
 		structureDsmMatrix = new StringBuilder[mFileList.size()][mFileList.size()];
-		typeList=mTypeList;
-		fileList=mFileList;
-		matrixBuilder=new StringBuilder();
-		typeBuilder=new StringBuilder();
-		fileListBuilder=new StringBuilder();
-		matrixCell=new StringBuilder();
-		for(int i=0;i<typeList.size();i++){
+		typeList = mTypeList;
+		fileList = mFileList;
+		matrixBuilder = new StringBuilder();
+		typeBuilder = new StringBuilder();
+		fileListBuilder = new StringBuilder();
+		matrixCell = new StringBuilder();
+		for (int i = 0; i < typeList.size(); i++) {
 			matrixCell.append("0");
 		}
-		
-		
+
 		buildString();
 	}
-	public GenerateDSM(List<String> mFileList){
-		
+
+	public GenerateDSM(List<String> mFileList) {
+
 		Collections.sort(mFileList);
-		historyDsmMatrix=new int[mFileList.size()][mFileList.size()];
-		fileList=mFileList;
-		fileListBuilder=new StringBuilder();
-		
+		historyDsmMatrix = new int[mFileList.size()][mFileList.size()];
+		fileList = mFileList;
+		fileListBuilder = new StringBuilder();
+
 		for (String fileName : fileList) {
 			fileListBuilder.append(fileName + "\n");
 		}
 	}
-	public void setFileStructureRelationType(String fromFileA,String toFileB,List<String> typeList2){
-		int x=fileList.indexOf(fromFileA);
-		int y=fileList.indexOf(toFileB);
+
+	public void setFileStructureRelationType(String fromFileA, String toFileB, List<String> typeList2) {
+		int x = fileList.indexOf(fromFileA);
+		int y = fileList.indexOf(toFileB);
 		if (structureDsmMatrix[x][y] == null) {
-			structureDsmMatrix[x][y] = new StringBuilder(
-					matrixCell.toString());
-			;
+			structureDsmMatrix[x][y] = new StringBuilder(matrixCell.toString());
 		}
-		for(String tmp:typeList2){
+		for (String tmp : typeList2) {
 			structureDsmMatrix[x][y].setCharAt(this.typeList.indexOf(tmp), '1');
 		}
 	}
-	public void setFileStructureRelationType(String fromFileA,String toFileB, String type){
-		int x=fileList.indexOf(fromFileA);
-		int y=fileList.indexOf(toFileB);
+
+	public void setFileStructureRelationType(String fromFileA, String toFileB, String type) {
+		int x = fileList.indexOf(fromFileA);
+		int y = fileList.indexOf(toFileB);
 		if (structureDsmMatrix[x][y] == null) {
 			structureDsmMatrix[x][y] = new StringBuilder(matrixCell.toString());
 		}
 		structureDsmMatrix[x][y].setCharAt(this.typeList.indexOf(type), '1');
 	}
-	public void setFileHistoryRelationCount(String fromFileA,String toFileB,int count){
-		int x=fileList.indexOf(fromFileA);
-		int y=fileList.indexOf(toFileB);
-		if(x==-1||y==-1){
+
+	public void setFileHistoryRelationCount(String fromFileA, String toFileB, int count) {
+		int x = fileList.indexOf(fromFileA);
+		int y = fileList.indexOf(toFileB);
+		if (x == -1 || y == -1) {
 			return;
 		}
-		historyDsmMatrix[x][y]=count;
-			
+		historyDsmMatrix[x][y] = count;
+
 	}
 
-	
-	private void buildString(){
+	private void buildString() {
 		typeBuilder.append("[");
 		for (int m = 0; m < typeList.size(); m++) {
 			typeBuilder.append(typeList.get(m) + ",");
@@ -91,26 +91,24 @@ public class GenerateDSM {
 		typeBuilder.append("]\n");
 		typeBuilder.append(fileList.size());
 		typeBuilder.append("\n");
-		
-		
+
 		for (String fileName : fileList) {
 			fileListBuilder.append(fileName + "\n");
 		}
 	}
-	
-	public void write2StructureDSM(String outputPath){
+
+	public void write2StructureDSM(String outputPath) {
 		try {
-			FileOutputStream fos = new FileOutputStream(
-					new File(outputPath));
+			FileOutputStream fos = new FileOutputStream(new File(outputPath));
 			fos.write(typeBuilder.toString().getBytes());
 			for (int m = 0; m < structureDsmMatrix.length; m++) {
-				matrixBuilder=new StringBuilder();
+				matrixBuilder = new StringBuilder();
 				for (int n = 0; n < structureDsmMatrix[0].length; n++) {
 					if (structureDsmMatrix[m][n] == null) {
 						matrixBuilder.append("0 ");
 					} else {
 						matrixBuilder.append(structureDsmMatrix[m][n].toString() + " ");
-						structureDsmMatrix[m][n]=null;
+						structureDsmMatrix[m][n] = null;
 					}
 				}
 				matrixBuilder.deleteCharAt(matrixBuilder.length() - 1);
@@ -125,16 +123,16 @@ public class GenerateDSM {
 			e.printStackTrace();
 		}
 	}
-	public void write2HistoryDSM(String outputPath){
+
+	public void write2HistoryDSM(String outputPath) {
 		try {
-			FileOutputStream fos = new FileOutputStream(
-					new File(outputPath));
-			String tmp=this.fileList.size()+"\n";
+			FileOutputStream fos = new FileOutputStream(new File(outputPath));
+			String tmp = this.fileList.size() + "\n";
 			fos.write(tmp.getBytes());
 			for (int m = 0; m < historyDsmMatrix.length; m++) {
-				matrixBuilder=new StringBuilder();
+				matrixBuilder = new StringBuilder();
 				for (int n = 0; n < historyDsmMatrix[0].length; n++) {
-						matrixBuilder.append(String.valueOf(historyDsmMatrix[m][n]) + " ");
+					matrixBuilder.append(String.valueOf(historyDsmMatrix[m][n]) + " ");
 				}
 				matrixBuilder.deleteCharAt(matrixBuilder.length() - 1);
 				matrixBuilder.append("\n");
@@ -148,6 +146,5 @@ public class GenerateDSM {
 			e.printStackTrace();
 		}
 	}
-	
 
 }
--- src/main/java/cn/edu/fudan/se/cochange_analysis/parser/RelationParser.java
+++ src/main/java/cn/edu/fudan/se/cochange_analysis/parser/RelationParser.java
@@ -21,55 +21,62 @@ import cn.edu.fudan.se.cochange_analysis.file.util.FileUtils;
 
 public class RelationParser {
 	private GitRepository repository;
-	private String project;
-	private String directory;
 
-	public RelationParser(GitRepository gitRepo, String project, String directory) {
-		this.project = project;
-		this.directory = directory;
+	public RelationParser(GitRepository repository) {
 		this.repository = repository;
-		this.repository = gitRepo;
-	}
-	public static void main(String args[]){
-//		GitRepository gitRepository = new GitRepository(1, "camel",
-//				"D:/echo/lab/research/co-change/projects/camel/.git");
-//		RelationParser a=new RelationParser(gitRepository,"camel","D:\\Workspace\\co-change summer\\understand\\camel\\");
-//		a.extractRelation();
-//		GitRepository gitRepository = new GitRepository(2, "cassandra",
-//				"D:/echo/lab/research/co-change/projects/cassandra/.git");
-//		RelationParser a=new RelationParser(gitRepository,"cassandra","D:\\Workspace\\co-change summer\\understand\\cassandra\\");
-//		a.extractRelation();
-//		GitRepository gitRepository = new GitRepository(3, "cxf",
-//				"D:/echo/lab/research/co-change/projects/cxf/.git");
-//		RelationParser a=new RelationParser(gitRepository,"cxf","D:\\Workspace\\co-change summer\\understand\\cxf\\");
-//		a.extractRelation();
-//		GitRepository gitRepository = new GitRepository(4, "hadoop",
-//		"D:/echo/lab/research/co-change/projects/hadoop/.git");
-//		RelationParser a=new RelationParser(gitRepository,"hadoop","D:\\Workspace\\co-change summer\\understand\\hadoop\\");
-//		a.extractRelation();
-//		GitRepository gitRepository = new GitRepository(5, "hbase",
-//				"D:/echo/lab/research/co-change/projects/hbase/.git");
-//		RelationParser a=new RelationParser(gitRepository,"hbase","D:\\Workspace\\co-change summer\\understand\\hbase\\");
-//		a.extractRelation();
-		GitRepository gitRepository = new GitRepository(6, "wicket",
-		"D:/echo/lab/research/co-change/projects/wicket/.git");
-RelationParser a=new RelationParser(gitRepository,"wicket","D:\\Workspace\\co-change summer\\understand\\wicket\\");
-a.extractRelation();
 	}
 
-	public void extractRelation() {
-		ArrayList<File> files = getListFiles(new File(directory));
+	public static void main(String args[]) {
+		// GitRepository gitRepository = new GitRepository(1, "camel",
+		// "D:/echo/lab/research/co-change/projects/camel/.git");
+		// RelationParser a=new
+		// RelationParser(gitRepository,"camel","D:\\Workspace\\co-change
+		// summer\\understand\\camel\\");
+		// a.extractRelation();
+		// GitRepository gitRepository = new GitRepository(2, "cassandra",
+		// "D:/echo/lab/research/co-change/projects/cassandra/.git");
+		// RelationParser a=new
+		// RelationParser(gitRepository,"cassandra","D:\\Workspace\\co-change
+		// summer\\understand\\cassandra\\");
+		// a.extractRelation();
+		// GitRepository gitRepository = new GitRepository(3, "cxf",
+		// "D:/echo/lab/research/co-change/projects/cxf/.git");
+		// RelationParser a=new
+		// RelationParser(gitRepository,"cxf","D:\\Workspace\\co-change
+		// summer\\understand\\cxf\\");
+		// a.extractRelation();
+		// GitRepository gitRepository = new GitRepository(4, "hadoop",
+		// "D:/echo/lab/research/co-change/projects/hadoop/.git");
+		// RelationParser a=new
+		// RelationParser(gitRepository,"hadoop","D:\\Workspace\\co-change
+		// summer\\understand\\hadoop\\");
+		// a.extractRelation();
+		// GitRepository gitRepository = new GitRepository(5, "hbase",
+		// "D:/echo/lab/research/co-change/projects/hbase/.git");
+		// RelationParser a=new
+		// RelationParser(gitRepository,"hbase","D:\\Workspace\\co-change
+		// summer\\understand\\hbase\\");
+		// a.extractRelation();
+		GitRepository gitRepository = new GitRepository(6, "wicket",
+				"D:/echo/lab/research/co-change/projects/wicket/.git");
+		RelationParser a = new RelationParser(gitRepository);
+		String inputDir = "D:\\Workspace\\co-change summer\\understand\\wicket\\";
+		a.extractRelation(inputDir);
+	}
+
+	public void extractRelation(String inputDir) {
+		ArrayList<File> files = getListFiles(new File(inputDir));
 		for (File f : files) {
 			if (f.getAbsolutePath().endsWith("xml")) {
 				System.out.println("Begin parse file " + f.getName());
-				parseFile(f);
+				parseFile(f, inputDir);
 				// System.exit(0);
 			}
 		}
 		// System.out.println(files.size());
 	}
 
-	private void parseFile(File f) {
+	private void parseFile(File f, String outputDir) {
 
 		String[] types = { "Extend", "Typed", "Import", "Call", "Create", "Cast", "Implement", "Set", "Modify", "Use",
 				"Throw" };
@@ -78,7 +85,7 @@ a.extractRelation();
 		for (String tmp : types) {
 			typeList.add(tmp);
 		}
-		GenerateDSM structureDSM=null;
+		GenerateDSM structureDSM = null;
 		GenerateDSM historyDSM;
 		HashMap<Integer, String> hm = new HashMap<Integer, String>();
 		SAXReader reader = new SAXReader();
@@ -97,42 +104,37 @@ a.extractRelation();
 					if (name.getValue().equals("longName")) {
 						String path = child.attribute("value").getValue();
 						// System.out.println(id + "\t" + parseName(path));
-						if(path.contains("\\test\\") || path.contains("\\tester\\")){
+						if (path.contains("\\test\\") || path.contains("\\tester\\")) {
 							break;
 						}
-						path=path.replace("\\", "/");
-						String packageName=FileUtils.parseFilePath(path, this.repository.getRepositoryName());
-						if(packageName.startsWith("D:/Workspace")){
-							int index=packageName.indexOf(this.repository.getRepositoryName());
-							packageName=packageName.substring(index+6);
-//							System.out.println(packageName);
+						path = path.replace("\\", "/");
+						String packageName = FileUtils.parseFilePath(path, this.repository.getRepositoryName());
+
+						String startsWithStr = "org/apache/";
+						if (this.repository.getRepositoryName().equals("hbase")) {
+							startsWithStr += "hadoop/hbase";
+						} else {
+							startsWithStr += this.repository.getRepositoryName();
 						}
-						String startsWithStr="org/apache/";
-						if(this.repository.getRepositoryName().equals("hbase")){
-							startsWithStr+="hadoop";
-						}else{
-							startsWithStr+=this.repository.getRepositoryName();
-						}
-						if(!packageName.startsWith(startsWithStr)){
+						if (!packageName.startsWith(startsWithStr)) {
 							break;
 						}
 						hm.put(id, packageName);
 						fileList.add(packageName);
 					}
-
 				}
 			}
 			Collections.sort(fileList);
-			System.out.println("FileListSize:"+fileList.size());
-			structureDSM=new GenerateDSM(typeList,fileList);
-			
+			System.out.println("FileListSize:" + fileList.size());
+			structureDSM = new GenerateDSM(typeList, fileList);
+
 			nodes = root.elements("edge");
 			for (Iterator<Element> it = nodes.iterator(); it.hasNext();) {
 				Element elm = (Element) it.next();
-				
+
 				int source = Integer.parseInt(elm.attribute("source").getValue());
 				int target = Integer.parseInt(elm.attribute("target").getValue());
-				if(!hm.containsKey(source)||!hm.containsKey(target)){
+				if (!hm.containsKey(source) || !hm.containsKey(target)) {
 					continue;
 				}
 				List<Element> children = elm.elements("att");
@@ -153,18 +155,19 @@ a.extractRelation();
 		} catch (DocumentException e) {
 			e.printStackTrace();
 		}
-		structureDSM.write2StructureDSM(this.directory+this.repository.getRepositoryName()+ "_structureDSM.dsm");
-		structureDSM=null;
-		historyDSM=new GenerateDSM(fileList);
-		List<FilePairCount> dataList=FilePairCountDAO.selectByRepositoryIdAndCount(this.repository.getRepositoryId(), 0);
-		for(FilePairCount item:dataList){
-			String[] files=item.getFilePair().split("\\|\\|");
-			historyDSM.setFileHistoryRelationCount(files[0],files[1], item.getCount());
-			historyDSM.setFileHistoryRelationCount(files[1],files[0], item.getCount());
-		
+		structureDSM.write2StructureDSM(outputDir + this.repository.getRepositoryName() + "_structureDSM.dsm");
+		structureDSM = null;
+		historyDSM = new GenerateDSM(fileList);
+		List<FilePairCount> dataList = FilePairCountDAO.selectByRepositoryIdAndCount(this.repository.getRepositoryId(),
+				0);
+		for (FilePairCount item : dataList) {
+			String[] files = item.getFilePair().split("\\|\\|");
+			historyDSM.setFileHistoryRelationCount(files[0], files[1], item.getCount());
+			historyDSM.setFileHistoryRelationCount(files[1], files[0], item.getCount());
+
 		}
-		historyDSM.write2HistoryDSM(this.directory+this.repository.getRepositoryName()+"_historyDSM.dsm");
-		
+		historyDSM.write2HistoryDSM(outputDir + this.repository.getRepositoryName() + "_historyDSM.dsm");
+
 	}
 
 	private ArrayList<File> getListFiles(File directory) {
