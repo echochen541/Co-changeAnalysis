d20ccd93f4567fb75a25aeb2221a359c19a2a2bd
Yelloman <cavinkfh@gmail.com>
2017-07-19 15:46:28 +0800

    ClusterReportParser

--- src/main/java/cn/edu/fudan/se/cochange_analysis/parser/ClusterReportParser.java
+++ src/main/java/cn/edu/fudan/se/cochange_analysis/parser/ClusterReportParser.java
@@ -1,7 +1,16 @@
 package cn.edu.fudan.se.cochange_analysis.parser;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -22,21 +31,84 @@ public class ClusterReportParser {
 				 "D:/echo/lab/research/co-change/projects/camel/.git");
 		 ClusterReportParser a=new ClusterReportParser(gitRepository);
 		 a.parse("camel_32_20_cluster..clsx","D:\\");
+		 System.out.println("Finished");
+		
+        
+       
 	}
 	public void parse(String fileName,String dir){
-		File f=new File(dir+File.separator+fileName);
-		SAXReader reader = new SAXReader();
-		Document document;
-		try{
-			document = reader.read(f);
-			Element root = document.getRootElement();
-			List<Element> elements=root.elements();
-			System.out.println("a");
-//			List<Element> nodes = root.elements("node");
-		} catch (DocumentException e) {
+		FileInputStream fis;
+		FileOutputStream fos;
+		Stack<String> stack=new Stack<String>();
+		List<String> groupLines=new ArrayList<String>();
+		try {
+			fis = new FileInputStream(new File(dir+File.separator+fileName));
+			fos=new FileOutputStream(new File(dir+File.separator+fileName.split("\\.")[0]+".csv"));
+			Scanner sc=new Scanner(fis);
+			int groupFlag=0;
+			
+			while(sc.hasNextLine()){
+				String line=sc.nextLine();
+				line=line.trim();
+				if(line.startsWith("<cluster")){
+					stack.push(line);continue;
+				}
+				if(line.startsWith("<group")){
+					stack.push(line);continue;
+				}
+				if(line.startsWith("</group>")){
+					stack.pop();
+					if(groupFlag!=0){
+						if(groupFlag>1){
+							for(String tmp:groupLines){
+								if(tmp.startsWith("<group")){
+									String res=match(tmp,"group","name")+",\n";
+									fos.write(res.getBytes());
+								}else if(tmp.startsWith("<item")){
+									String res=match(tmp,"item","name")+",\n";
+									fos.write(res.getBytes());
+								}
+								
+							}
+							fos.write("\n".getBytes());
+						}
+						groupLines.clear();
+						groupFlag=0;
+//						System.out.println(line);
+					}
+					continue;
+				}
+				if(line.startsWith("<item")){
+					String group=stack.peek();
+					if(groupFlag==0){
+						groupLines.add(group);
+//						System.out.println(group);
+						
+					}
+					groupFlag++;
+					groupLines.add(line);
+//					System.out.println(line);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		
 	}
-
+	public static String match(String source, String element, String attr) {
+        String result=null;
+        String reg = "<" + element + " " + attr +"="+ "\"(.*)\"";
+        Matcher m = Pattern.compile(reg).matcher(source);
+        while (m.find()) {
+            String r = m.group(1);
+            result=r;
+            return result;
+        }
+        return result;
+    }
 }
